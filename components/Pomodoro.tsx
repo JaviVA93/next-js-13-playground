160e8d256ab35dde05f2f43748704afbfb7917cf
Javi Villar <javier.villar@makingscience.com>
2022-11-20 21:04:16 +0100

    remove old interval  declaration

--- components/Pomodoro.tsx
+++ components/Pomodoro.tsx
@@ -166,7 +166,6 @@ export default function Pomodoro() {
 
             setPlayPauseButtonBackground('pause');
 
-            const auxInt = setInterval(updatePomodoro, 250)
             pomoInterval.current = setInterval(updatePomodoro, 250);
         }
     }
